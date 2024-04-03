676204a2eb6c732e4165f02c1add4b50a7c93667
djuric24 <djuric24a@gmail.com>
2024-04-03 18:16:10 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 export const App = () => {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
-      const apiUrl = "http://localhost:3111"
+    const apiUrl = "http://localhost:3111";
+    const [access, setAccess] = useState(true);
 
     const userInput = (event) => {
         const newUser = event.target.value;
@@ -27,14 +28,37 @@ export const App = () => {
      password : password
     }
      let res = axios.post(apiUrl+"/savedata",newUser);
+     console.log(res);
+     setAccess(true);
     //axios.post
     //axios.get
    } 
+if(access === true) {
+    <form className="form">
+        <p className="title">Log in </p>
+        <p className="message">Log in now and get full access to our app. </p>
+        <div className="flex">
+        <label>
+        <input className="input" value={userName} onChange={userInput} type="text" placeholder="" required=""/>
+            <span>Firstname</span>
+        </label>
+        </div>  
+        <div className="flex">
+            <label>
+            <input className="input" value={password} onChange={passwordInput} type="password" placeholder="" required=""/>
+              <span>Password</span>
+            </label>   
+        </div>
+        <div className="flex">
+            <button className="submit" onClick={register}>Sign up</button>
+        </div>
+</form>
+}
 
 return (
 <form className="form">
     <p className="title">Register </p>
-    <p className="message">Signup now and get full access to our app. </p>
+    <p className="message">Sign in now and get full access to our app. </p>
         <div className="flex">
         <label>
             <input className="input" value={userName} onChange={userInput} type="text" placeholder="" required=""/>
@@ -48,7 +72,7 @@ return (
             </label>   
         </div>
         <div className="flex">
-            <button className="submit" onClick={register}>Submit</button>
+            <button className="submit" onClick={register}>Sign in</button>
         </div>
 </form>
     )
