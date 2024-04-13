ffcd60437e266b1bf7c7d43e0795f2cb346b163a
djuric24 <djuric24a@gmail.com>
2024-04-13 15:34:43 +0200

    todoDis

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 export const Home = ({setCurrentPage, currentUser}) => {
-  console.log(currentUser);
   return (
-    <div>
-        <h1>You are home</h1>
-        <h3>{currentUser.todos}</h3>
+    <div className='home'>
+      <div className='nav'>
+        <h2>Welcome: { currentUser.username} </h2>
         <button onClick={() => (setCurrentPage("login"))} >Log out</button>
+      </div>
+        <h3>Your todos are :</h3>
+        {currentUser.todos.map((todo) => {
+          return (
+          <div key={todo.id}>
+            <p>- {todo.text}</p>
+          </div>
+          )
+        })}   
     </div>
   )
 }   
-
-
